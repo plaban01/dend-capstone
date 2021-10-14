4130140c8488bcec01f035408ca1edb3d81afdf8
Plaban Das <plaban.das01@gmail.com>
2021-10-14 09:02:00 +0530

    chore: Add plugins package
    Add plugins package with necessary imports
    
    Signed-off-by: Plaban Das <plaban.das01@gmail.com>

---
+++ airflow/plugins/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import division, absolute_import, print_function
+
+from airflow.plugins_manager import AirflowPlugin
+
+import operators
+import helpers
+
+# Defining the plugin class
+class UdacityPlugin(AirflowPlugin):
+    name = "udacity_plugin"
+    operators = [
+        operators.StageToRedshiftOperator,
+        operators.LoadTableOperator,
+        operators.DataQualityOperator
+    ]
+    helpers = [
+        helpers.SqlQueries
+    ]
